ac4ece85becf78aa4a2a14a4a2dbd1e6ab58447f
Anubhav <anubhavpal15june@gmail.com>
2023-05-05 16:06:29 +0530

    Add the containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Article, Brand, Feature, Navbar, CTA, } from './components'
+import { Blog, Features, Footer, Header, Possibility, whatGPT3 } from './containers'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <whatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
+
 
 export default App
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './possibility.css'
+
+
+const Possiblity = () => {
+  return (
+    <div>Possiblity</div>
+  )
+}
+
+export default Possiblity
